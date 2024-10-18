2958f12adbe9bdab9adb4855eaece24fe01b24ee
SreckoJuricevic <sba24199@student.cct.ie>
2024-10-18 10:42:26 +0100

    fields and getters for CA1Customer created, along with method called from constructor to calculate a value

--- src/CA1Customer.java
+++ src/CA1Customer.java
@@ -70,3 +70,64 @@ public class CA1Customer {
    public void setFinalValue(double finalValue) {
       this.finalValue = finalValue;
    }
+
+   //This method is called by the constructor after the fields have been initialized ,
+   //it calculates needed values and initializes the total finalValue field, later printed to the file
+   private void calculateDiscount(){
+      int currentYear = 2024;
+      double discountPercentage = 0.0;
+
+      //------------CLASS 1----------------
+      if (customerClass == 1) {
+         if (lastPurchaseYear == currentYear) {
+            discountPercentage = 30.0;    //Class = 1 and Last Purchase in 2024
+
+         } else if ( lastPurchaseYear < currentYear &&  lastPurchaseYear >= (currentYear - 5)) {
+            discountPercentage = 20.0;  //Class = 1 and Last Purchase before than 2024 , within 5y
+
+         } else if (lastPurchaseYear < (currentYear - 5)) {
+            discountPercentage = 10.0;  //Class 1, no purchase in the last 5 years
+         }
+
+         //------------CLASS 2----------------
+      } else if (customerClass == 2) {
+         if (lastPurchaseYear == currentYear) {
+            discountPercentage = 15.0;  //Class 2, purchase in 2024
+
+         } else if (lastPurchaseYear < currentYear && lastPurchaseYear >= (currentYear - 5)) {
+            discountPercentage = 13.0;  // Class 2, purchase before 2024 but within 5 years
+
+         } else if (lastPurchaseYear < (currentYear - 5)) {
+            discountPercentage = 5.0;   // Class 2, no purchase in the last 5 years
+         }
+
+         //------------CLASS 3----------------
+      } else if (customerClass == 3) {
+         if (lastPurchaseYear == currentYear) {
+            discountPercentage = 3.0;   // Class 3, purchase in 2024
+
+         } else if (lastPurchaseYear < currentYear) {
+            discountPercentage = 0.0;   //Class 3, no discount for purchases before 2024
+         }
+      }
+      //calculating how much should we subtract from the total purchase amount
+      double discountAmount = totalPurchase * discountPercentage/100;
+      //initializing the field
+      this.finalValue = totalPurchase - discountAmount;
+
+   }//calculate discount end
+
+
+   //OVERRIDING THE TO-STRING METHOD
+   @Override
+   public String toString() {
+      return "Customer{" +
+         "firstName='" + firstName + '\'' +
+         ", secondName='" + secondName + '\'' +
+         ", totalPurchase=" + totalPurchase +
+         ", customerClass=" + customerClass +
+         ", lastPurchaseYear=" + lastPurchaseYear +
+         ", final value after discount=" + finalValue +
+         '}';
+   }
+}
